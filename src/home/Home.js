189db799acf05b413793c79274bc444b568260dc
CostinRaduIonut <ionutz10_costin@yahoo.com>
2023-01-10 16:47:10 +0200

    Front-2

---
+++ src/home/Home.js
@@ -0,0 +1,350 @@
+import React, { ChangeEvent, SyntheticEvent, useEffect, useState } from "react";
+import { BrowserRouter as Router, Link, useNavigate } from "react-router-dom";
+import "./Home.css";
+import { toast } from "react-toastify";
+import SockJsClient from "react-stomp";
+
+const SOCKET_URL = "http://localhost:8080/ws-chat/";
+
+export const Home = () => {
+  const [message, setMessage] = useState();
+  let onConnected = () => {
+    console.log("Connected!!");
+  };
+
+  let onMessageReceived = (msg) => {
+    console.log("New Message Received!!", msg);
+    setMessage(msg.sender);
+    toast.warning("userul " + message + " a castigat o licitatie");
+  };
+
+  let onSendMessage = (id, castigator, pret) => {
+    const requestOptions1 = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+      body: JSON.stringify({
+        id:id,
+        sender: castigator,
+        content: pret,
+      }),
+    };
+
+    fetch(`http://localhost:8080/api/send`, requestOptions1)
+      .then((response) => response.text())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+  const [data, setData] = useState([]);
+
+  const convertToDataNormal = (timp) => {
+    const date = new Date(timp);
+    var year = date.getFullYear();
+    var month = ("0" + (date.getMonth() + 1)).slice(-2);
+    var day = ("0" + date.getDate()).slice(-2);
+
+    return `${day}-${month}-${year}`;
+  };
+
+  useEffect(() => {
+    try {
+      setInterval(() => {
+
+      fetch("http://localhost:8080/products", {
+        headers: {
+          method: "GET",
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
+        .then((response) => response.json())
+        .then((usefulData) => {
+          setData(usefulData);
+        });
+      }, 2000);
+    } catch (error) {
+      toast.error("Eroare la afisarea datelor");
+    }
+  }, []);
+
+  function setExpirat(data, id, castigator, pret) {
+    try {
+      const dataDeverificat = Date.now();
+      const rez = parseInt(data) + 172800000;
+      //de modificat din false in true ca sa mearga
+      if (rez > dataDeverificat) {
+      } else {
+        onSendMessage(id, castigator, pret);
+
+        const requestOptions = {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage.getItem("token"),
+          },
+          body: JSON.stringify({ expirat: true }),
+        };
+        fetch(`http://localhost:8080/products/expirat/${id}`, requestOptions);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const LogOut = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("emailUser");
+    localStorage.removeItem("idUser");
+    localStorage.removeItem("pret");
+    window.location.reload();
+  };
+
+  const navigate = useNavigate();
+  const pressLogin = () => {
+    navigate("/Login");
+  };
+  const pressRegister = () => {
+    navigate("/Register");
+  };
+
+  const [price, setPrice] = useState(undefined);
+
+  const handlePriceChange = (e) => {
+    setPrice(parseInt(e.target.value));
+  };
+
+  const categories = ["Mar", "Banana", "Para", "Pruna"];
+
+  const [checked, setChecked] = useState(
+    new Array(categories.length).fill(false)
+  );
+
+  const handleChange = (idx) => {
+    const newChecked = checked.map((checkedBool, checkedIndex) =>
+      checkedIndex === idx ? !checkedBool : checkedBool
+    );
+
+    setChecked(newChecked);
+  };
+  const [searched, setSearched] = useState("");
+
+  const [afis, setAfis] = useState(false);
+  const [textAfis, setTextAfis] = useState("User");
+  const afiseaza = () => {
+    setAfis(!afis);
+    if (afis === false) setTextAfis("Hide");
+    else setTextAfis("User");
+  };
+
+  return (
+    <>
+      <SockJsClient
+        url={SOCKET_URL}
+        topics={["/topic/group"]}
+        onConnect={onConnected}
+        onDisconnect={console.log("Disconnected!")}
+        onMessage={(msg) => onMessageReceived(msg)}
+        debug={false}
+      />
+      <div className="home-body">
+        <div className="home-title">Piata mica</div>
+        <div className="home-top">
+          <div className="home-top-searchbar">
+            <div className="searchbar-body">
+              <form>
+                <input
+                  value={searched}
+                  onChange={(event) => setSearched(event.target.value)}
+                  className="searchbar-input"
+                  name="query"
+                  type="text"
+                  placeholder="Cauta dupa nume..."
+                />
+              </form>
+            </div>{" "}
+          </div>{" "}
+          {localStorage.getItem("token") ? (
+            <>
+              <div className="home-top-actionbutton">
+                <button onClick={() => navigate("/Upload")}>Upload</button>
+              </div>
+              <div className="home-top-actionbutton">
+                <button onClick={() => navigate("/MyProducts")}>My prod</button>
+              </div>
+              <div className="home-top-actionbutton">
+                <button onClick={LogOut}>Logout</button>
+              </div>
+            </>
+          ) : (
+            afis && (
+              <>
+                <div className="home-top-actionbutton">
+                  <button onClick={pressLogin}>Login</button>
+                </div>
+                <div className="home-top-actionbutton">
+                  <button onClick={pressRegister}>Inregistrare</button>
+                </div>
+              </>
+            )
+          )}
+          {!localStorage.getItem("token") ? (
+            <div className="home-top-actionbutton">
+              <button onClick={afiseaza}>{textAfis}</button>
+            </div>
+          ) : (
+            <></>
+          )}
+        </div>
+        <div className="home-main">
+          {localStorage.getItem("token") ? (
+            <>
+              <div className="home-main-column">
+                <div className="home-main-column-title">
+                  <h1>Filtrare</h1>
+                </div>
+                <form>
+                  <div className="filter-section">
+                    <div className="filter-section-price">
+                      <span>Pret:</span>
+                      <input
+                        value={price || 0}
+                        onChange={handlePriceChange}
+                        min="0"
+                        name="pret"
+                        type="number"
+                      />
+                    </div>
+                    <div className="filter-section-note">
+                      Pretul ales:{" "}
+                      {price !== undefined && price > 0 && (
+                        <>
+                          <span
+                            style={{ color: "darkgreen", fontWeight: "500" }}
+                          >
+                            {price}
+                          </span>{" "}
+                          RON
+                        </>
+                      )}
+                    </div>
+                  </div>
+                  <div className="filter-section">
+                    <label htmlFor="categorie">Categorii:</label>
+                    <div className="filter-section-categories">
+                      {categories.map((category, index) => {
+                        return (
+                          <div key={index}>
+                            <input
+                              key={index}
+                              type="checkbox"
+                              name="categorie"
+                              value={category}
+                              checked={checked[index]}
+                              onChange={() => handleChange(index)}
+                            />
+                            {category}
+                          </div>
+                        );
+                      })}
+                    </div>
+                    <div className="filter-section-note">
+                      Categorii alese:{" "}
+                      <span style={{ fontWeight: "500" }}>
+                        {categories
+                          .filter((_cat, idx) => checked[idx])
+                          .join(", ")}
+                      </span>
+                    </div>
+                  </div>
+                </form>
+              </div>
+              {/* incepe sectiunea pentru carduri */}
+
+              {data
+                .filter((data) => data.expirat == false)
+                .filter((data) => data.pret >= (price || 0))
+                .filter((data) =>
+                  data.categorie.includes(
+                    categories
+                      .filter((_cat, idx) => checked[idx])
+                      .join("")
+                      .toLowerCase()
+                  )
+                )
+                .filter((data) => data.name.includes(searched)|| data.categorie.includes(searched))
+                .sort((data, data1) => data.id - data1.id)
+                .map((data) => {
+                  {
+                    setExpirat(data.data, data.id, data.castigator, data.pret);
+                  }
+
+                  return (
+                    <div className="home-main-column" key={data.id}>
+                      <div className="shell">
+                        <div className="container">
+                          <div className="row">
+                            <div className="col-md-3">
+                              <div className="wsk-cp-product">
+                                <div className="wsk-cp-img">
+                                  <img src={data.img} alt="" />{" "}
+                                </div>
+                                <div className="wsk-cp-text">
+                                  <div className="category">
+                                    <span>{data.categorie}</span>
+                                  </div>
+                                  <div className="title-product">
+                                    <h3>{data.name}</h3>
+                                  </div>
+                                  <div className="description-prod">
+                                    <p>{data.descriere}</p>
+                                    <br></br>
+                                    <p>
+                                      {convertToDataNormal(parseInt(data.data))}
+                                    </p>
+                                  </div>
+                                  <div className="card-footer">
+                                    <div className="wcf-left">
+                                      <span className="price">
+                                        {data.pret}RON
+                                      </span>
+                                    </div>
+                                    <div className="wcf-right">
+                                      <Link
+                                        style={{ padding: "2%" }}
+                                        to={`/Product/${data.id}`}
+                                      >
+                                        Liciteaza
+                                      </Link>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+            </>
+          ) : (
+            <>
+              <div className="home" style={{ fontSize: "40px" }}>
+                <h1>PLEASE </h1>
+              </div>
+              <div className="home" style={{ fontSize: "40px" }}>
+                <h1>LOGIN</h1>
+              </div>
+            </>
+          )}
+          {/* final sectiune carduri */}
+        </div>
+      </div>
+
+      <div className="footer">Costin Radu Ionut</div>
+    </>
+  );
+};
---
+++ src/product/UploadProduct.js
@@ -0,0 +1,170 @@
+import React, { SyntheticEvent, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import "./UploadProduct.css";
+import logo from "./auction.png";
+
+const Upload = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+  
+  },[]);
+  const [nume, setNume] = useState("");
+  const [pret, setPret] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [descriere, setDescriere] = useState("");
+  const [img, setImg] = useState("");
+  //   const formData = new FormData();
+
+  const handleSubmit = async (event) => {
+    try {
+      event.preventDefault();
+      var idOwner = localStorage.getItem("idUser");
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+        body: JSON.stringify({
+          id_owner: idOwner,
+          name: nume,
+          pret: pret,
+          categorie: categoria,
+          descriere: descriere,
+          img: "prod1.png",
+          data: "",
+        }),
+      };
+      fetch("http://localhost:8080/products", requestOptions);
+      // .then((response) =>
+      // response.json()
+      //);
+      toast.success("Produs adaugat cu succes");
+      navigate("/");
+    } catch (err) {
+      console.error(err);
+      toast.error("Ceva a mers prost");
+    }
+  };
+
+  return (
+    <div className="login-body">
+      <div className="login-title">Adauga</div>
+      <div className="login-top">
+        <div className="login-btn">
+          <button
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Inapoi
+          </button>
+        </div>
+      </div>
+      <div className="login-main" style={{ padding: "10px" }}>
+        <form>
+          <div className="row">
+            <div className="col-25">
+              <label htmlFor="nume">Nume produs</label>
+            </div>
+            <div className="col-75">
+              <input
+                type="text"
+                id="nume"
+                name="nume"
+                placeholder="Introdu nume"
+                required={true}
+                value={nume}
+                onChange={(event) => setNume(event.target.value)}
+              ></input>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-25">
+              <label htmlFor="pret">Pret</label>
+            </div>
+            <div className="col-75">
+              <input
+                style={{ width: "100%" }}
+                type="number"
+                id="pret"
+                name="pret"
+                placeholder="Introdu pret"
+                required={true}
+                value={pret}
+                onChange={(event) => setPret(event.target.value)}
+              ></input>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-25">
+              <label htmlFor="categoria">Categorie</label>
+            </div>
+            <div className="col-75">
+              <select
+                name="categotia"
+                id="categoria"
+                value={categoria}
+                onChange={(event) => setCategoria(event.target.value)}
+              >
+                <option></option>
+                <option value="mar">Mar</option>
+                <option value="banana">Banana</option>
+                <option value="para">Para</option>
+                <option value="pruna">Pruna</option>
+              </select>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-25">
+              <label htmlFor="descriere">Descriere</label>
+            </div>
+            <div className="col-75">
+              <textarea
+                id="descriere"
+                name="descriere"
+                placeholder="Descrie produsul"
+                required={true}
+                value={descriere}
+                onChange={(event) => setDescriere(event.target.value)}
+              ></textarea>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-25">
+              <label htmlFor="img">Imagine</label>
+            </div>
+            <div className="col-75">
+              <input
+                type="file"
+                id="img"
+                name="img"
+                required={true}
+                value={img}
+                onChange={(event) => setImg(event.target.value)}
+              ></input>
+            </div>
+          </div>
+          <div className="row">
+            <button
+              type="button"
+              style={{ margin: "20px 30%", padding: "10px" }}
+              value="submit"
+              onClick={handleSubmit}
+            >
+              Inregistrare produs
+            </button>
+          </div>
+        </form>
+        <div className="login-main-column">
+          <div className="imag" style={{ padding: "20px" }}>
+            <img src={logo} style={{ width: "70%" }}></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Upload;
